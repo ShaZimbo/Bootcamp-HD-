43e171c85de6f7c689cd632259b0bd6c2784ce01
ShaZimbo <shanwratten@gmail.com>
2025-01-20 10:19:40 +0000

    Add files via upload

---
+++ details.py
@@ -0,0 +1,12 @@
+# Request user full name
+user_Name=input("Please provide your full name: ")
+# Request user age
+user_Age=input("Please provide your age: ")
+# Request user house number
+user_HseNum=input("Please provide your house number: ")
+# Request user street name
+user_StNm=input("Please provide the name of your street: ")
+# Print all variables
+print(f"This is {user_Name}. "
+      f"He/She is {user_Age} years old "
+      f"and lives at {user_HseNum} {user_StNm}.")
